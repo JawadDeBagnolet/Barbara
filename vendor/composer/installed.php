3d9337a9142ec81d804f25e8bfe92fb03e807ab3
JawadDeBagnolet <cjawad.a@gmail.com>
2026-02-05 03:38:25 +0100

    ajout img + modif 3 premieres enveloppes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f888478006776ae113066efcb849b9a17f4f9272',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f888478006776ae113066efcb849b9a17f4f9272',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
